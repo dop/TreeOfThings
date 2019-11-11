c5b6f27a8b325a10f55f0ea41a2aa4534be82807
Donatas Petrauskas <donatas.petr@gmail.com>
2019-11-11 11:45:37 +0200

    Initial Commit

---
+++ TreeOfThings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TreeOfThings
+//
+//  Created by Donatas Petrauskas on 2019-11-11.
+//  Copyright © 2019 Donatas Petrauskas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
